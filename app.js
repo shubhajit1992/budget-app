f3ff8a99ccea030ec6c0d590dfb861f9094d215e
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-03-20 09:53:59 +0530

    Implementing the module pattern

---
+++ app.js
@@ -0,0 +1,16 @@
+/* Module Pattern - Keep pieces of code that are related to one another together inside separate, independent and organized units. Each of these modules will have variables and functions that are private i.e. it's only accessible inside of a module so that no other code can override our data. Our data and code is safe. Besides private variables and methods, we are also going to have public methods which means we expose them to public so that other functions or modules can access and use them. This is called data encapsulation.*/
+
+// BUDGET CONTROLLER
+var budgetController = (function () {
+
+})();
+
+// UI CONTROLLER
+var uiController = (function () {
+
+})();
+
+// GLOBAL APP CONTROLLER
+var appController = (function (budgetCtrl, uiCtrl) {
+
+})(budgetController, uiController);
